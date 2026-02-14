102eecb30754ce95e3c0da14cb913293ca373afb
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-14 21:00:42 +0100

    added delete route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() //Get all users
+  findAll() {
+    return 'This action returns all users';
+  }
+
+  @Get('interns') //Get all interns
+  findAllInterns() {
+    return 'This action returns all interns';
+  }
+
+  @Get(':id') //Get user by id
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} user`;
+  }
+
+  @Post() //Post /Users
+  create(@Body() user: unknown) {
+    return user;
+  }
+
+  @Patch(':id') //Update user by id
+  update(@Param('id') id: string, @Body() user: any) {
+    return `This action updates a #${id} user with data: ${JSON.stringify(user)}`;
+  }
+
+  @Delete(':id') //Delete user by id
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} user`;
+  }
+}
